2e4f32462c22cc25e4b1c7694bceafa852c4a838
Maxouille <xd.maxouille@gmail.com>
2022-04-20 16:25:12 +0200

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-var isExtensionOn = true;  //this will be changed by popup.js directly.
-// Subscribe to onVisited event, so that injectSite() is called once at every pageload.
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-   if (changeInfo.status == 'complete') {
-      console.log("COMPLETE")
-         chrome.tabs.sendMessage(tab.id,{action:isExtensionOn});
-   }
-});
