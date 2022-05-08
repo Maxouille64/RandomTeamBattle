737691309344ed6a693ffe01629f2a844a444ea0
Maxouille <xd.maxouille@gmail.com>
2022-05-08 18:53:58 +0200

    Update content.js

--- chrome/content.js
+++ chrome/content.js
@@ -9,17 +9,31 @@
 // ==/UserScript==
 
 var loaded = false;
-
 function load(file) {
-	let s = document.createElement('script');
-	s.src = chrome.runtime.getURL(file);
-	s.onload = function() {
-		this.remove();
-	};
-	(document.head || document.documentElement).appendChild(s);
+	let elm = document.createElement("script")
+	elm.src = chrome.runtime.getURL("/" + file)
+	document.body.appendChild(elm)
 };
-chrome.storage.sync.get('enabled',val => {
-	if (val.enabled === '') return;
+chrome.storage.sync.get('enabled', val => {
+	if (val.enabled === '' || loaded == true) return;
+	loaded = true
 	load("production.min.js");
 	load("showdown.js");
+	const p = document.createElement("center");
+	const bouton = document.createElement("button");
+	const titre = document.createElement("strong");
+	const soustitre = document.createElement("small");
+	const br = document.createElement("br");
+	bouton.id = "rtb";
+	bouton.className = "button mainmenu";
+	bouton.name = "rtb";
+	bouton.setAttribute("onclick","ConsoleRoom.prototype.customCommands['rtb'](null,document.getElementsByName('format')[0].value);");
+	titre.innerText = "Battle!";
+	soustitre.innerText = "with a randomized team";
+
+	bouton.appendChild(titre);
+	bouton.appendChild(br);
+	bouton.appendChild(soustitre);
+	p.appendChild(bouton);
+	document.getElementsByClassName('battleform')[0].appendChild(p);
 });
