fbed6b51a6eef8b69acc119bd4209acfb48941fa
Maxouille <xd.maxouille@gmail.com>
2022-06-06 18:06:08 +0200

    Update content.js

--- chrome/content.js
+++ chrome/content.js
@@ -29,11 +29,14 @@ chrome.storage.sync.get('enabled', val => {
 	const battleform = document.getElementsByClassName('battleform')[0];
 	bouton.id = "rtb";
 	bouton.className = "button mainmenu";
+	//p2.className = "cancel buttonbar";
+	bouton2.id = "rtbcancelSearch";
 	bouton.name = "rtb";
 	bouton2.name = "send";
 	bouton2.type = "submit";
 	bouton2.value = "/cancelSearch";
-	bouton2.setAttribute("onclick","cancel = true;");
+	bouton2.setAttribute("onclick","cancel_rtb();");
+	bouton2.setAttribute("style","display: none");
 	bouton2.innerText = "Cancel";
 	bouton.setAttribute("onclick","ConsoleRoom.prototype.customCommands['rtb'](null,document.getElementsByName('format')[0].value);");
 	titre.innerText = "Battle!";
